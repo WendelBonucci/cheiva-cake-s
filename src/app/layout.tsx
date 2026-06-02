d56a5d4d49a56c07af507ce1f4296c6995a75ce7
Wendell Eduardo Albuquerque Bonucci <contact.wendellbonucci@gmail.com>
2026-06-02 11:45:54 -0300

    Criação da estrutura do Projeto - Base

---
+++ src/app/layout.tsx
@@ -0,0 +1,72 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata = {
+  title: "Cheiva Cake'S | Confeitaria de Bolos, Doces e Sobremesas",
+
+  description:
+    "A Cheiva Cake'S é especializada na produção de bolos artesanais, doces finos, sobremesas e encomendas para festas, aniversários, casamentos e eventos especiais. Sabor, qualidade e carinho em cada detalhe.",
+
+  keywords: [
+    "Confeitaria",
+    "Bolos Artesanais",
+    "Doces Finos",
+    "Sobremesas",
+    "Bolos Personalizados",
+    "Bolos para Aniversário",
+    "Bolos para Casamento",
+    "Doces para Festa",
+    "Encomenda de Bolos",
+    "Cupcakes",
+    "Brigadeiros Gourmet",
+    "Confeitaria Artesanal",
+    "Mesa de Doces",
+    "Doceria",
+    "Cheiva Cakes"
+  ],
+
+  authors: [
+    {
+      name: "Cheiva Cake'S",
+      url: "https://seusite.com.br",
+    },
+  ],
+
+  icons: {
+    icon: "/logoHeader.png",
+  },
+
+  openGraph: {
+    title: "Cheiva Cake'S | Bolos, Doces e Sobremesas Artesanais",
+    description:
+      "Encomende bolos personalizados, doces finos e sobremesas artesanais para tornar seus momentos ainda mais especiais. Qualidade, sabor e beleza em cada criação.",
+
+    url: "https://seusite.com.br",
+
+    siteName: "Cheiva Cake'S",
+
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Cheiva Cake'S - Confeitaria Artesanal",
+      },
+    ],
+
+    locale: "pt_BR",
+    type: "website",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR"className={''}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
